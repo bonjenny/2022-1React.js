1c593a9ddaac456a552981927f76746baa48b917
엄지희 <bonjenny1@gmail.com>
2022-05-13 14:26:52 +0900

    5/13-3_CreateCustomerComponent

---
+++ management/src/components/Customer.js
@@ -0,0 +1,17 @@
+// src/Customer.js
+import React from 'react';
+
+class Customer extends React.Component {
+    render() {
+        return(
+            <div>
+                <h2>엄지희</h2>
+                <p>020224</p>
+                <p>여자</p>
+                <p>대학생</p>
+            </div>
+        )
+    }
+}
+
+export default Customer;
